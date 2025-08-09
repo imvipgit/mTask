a75b191bc53f10386db65921ab08a381a5db5a9b
Vipul Sindha <vipul.sindha@icloud.com>
2025-08-09 12:55:05 +0200

    Initial Commit

---
+++ mTask/mTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  mTaskApp.swift
+//  mTask
+//
+//  Created by Vipul Sindha on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct mTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mTaskTests/mTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  mTaskTests.swift
+//  mTaskTests
+//
+//  Created by Vipul Sindha on 09/08/2025.
+//
+
+import Testing
+@testable import mTask
+
+struct mTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
